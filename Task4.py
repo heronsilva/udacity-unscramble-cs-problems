bd629bc5ccc9cc131a99cbe55df981558f015787
Heron Silva <heron@vizir.com.br>
2021-05-21 02:12:17 -0300

    fix: change the logic for finding possible telemarketers

--- Task4.py
+++ Task4.py
@@ -3,7 +3,6 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files.
 """
 import csv
-import re
 
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
@@ -26,39 +25,31 @@ Print a message:
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
 
-telemarketing_line_number_pattern = r"^140"
-possible_telemarketers = []
-
-call_incoming_numbers = []
-call_receiving_numbers = []
-text_incoming_numbers = []
-text_receiving_numbers = []
+callers = []
+called = []
+texters = []
+texted = []
 
 for call in calls:
-    incoming_number = call[0]
-    receiving_number = call[1]
+    incoming_call_number = call[0]
+    receiving_call_number = call[1]
 
-    if re.match(telemarketing_line_number_pattern, incoming_number):
-        call_incoming_numbers.append(incoming_number)
+    if incoming_call_number not in callers:
+        callers.append(incoming_call_number)
 
-    call_receiving_numbers.append(receiving_number)
+    called.append(receiving_call_number)
 
 for text in texts:
-    incoming_number = text[0]
-    receiving_number = text[1]
+    incoming_text_number = text[0]
+    receiving_text_number = text[1]
 
-    text_incoming_numbers.append(incoming_number)
-    text_receiving_numbers.append(receiving_number)
+    texters.append(incoming_text_number)
+    texted.append(receiving_text_number)
 
-for phone_number in call_incoming_numbers:
-    if phone_number in call_receiving_numbers \
-            or phone_number in text_incoming_numbers \
-            or phone_number in text_receiving_numbers:
-        continue
+for phone_number in callers[:]:
+    if phone_number in called or phone_number in texters or phone_number in texted:
+        callers.remove(phone_number)
 
-    if phone_number not in possible_telemarketers:
-        possible_telemarketers.append(phone_number)
+possible_telemarketers = sorted(callers)
 
-sorted_possible_telemarketers_list = sorted(possible_telemarketers)
-
-print("These numbers could be telemarketers:", *sorted_possible_telemarketers_list, sep="\n")
+print("These numbers could be telemarketers:", *possible_telemarketers, sep="\n")
