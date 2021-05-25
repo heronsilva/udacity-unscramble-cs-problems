5ebd4caa28305d46764755403e4246ac176e5dc3
Heron Silva <heron@vizir.com.br>
2021-05-25 03:24:46 -0300

    refactor: replace default list with set

--- Task1.py
+++ Task1.py
@@ -19,26 +19,20 @@ Print a message:
 "There are <count> different telephone numbers in the records."
 """
 
-telephone_numbers = []
+telephone_numbers = set()
 
 for text in texts:
     incoming_number = text[0]
     receiving_number = text[1]
 
-    if incoming_number not in telephone_numbers:
-        telephone_numbers.append(incoming_number)
-
-    if receiving_number not in telephone_numbers:
-        telephone_numbers.append(receiving_number)
+    telephone_numbers.add(incoming_number)
+    telephone_numbers.add(receiving_number)
 
 for call in calls:
     incoming_number = call[0]
     receiving_number = call[1]
 
-    if incoming_number not in telephone_numbers:
-        telephone_numbers.append(incoming_number)
-
-    if receiving_number not in telephone_numbers:
-        telephone_numbers.append(receiving_number)
+    telephone_numbers.add(incoming_number)
+    telephone_numbers.add(receiving_number)
 
 print("There are {} different telephone numbers in the records.".format(len(telephone_numbers)))
